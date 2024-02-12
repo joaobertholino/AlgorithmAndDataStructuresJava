b49fabf3dd68cb6ee390760dc6739a78d0e2656d
João Bertholino <joaobertholino964@gmail.com>
2024-02-12 20:45:58 -0300

    ...

---
+++ src/VetoresAndArrays/Introduction/VectorsAndArrays.java
@@ -0,0 +1,18 @@
+package VetoresAndArrays.Introduction;
+
+/**
+ * Um vetor, qua tambem opde ser chamado de array ou listas é a estrutura de dados
+ * mais simples que existe.
+ * <p>
+ * Ela armazena uma serie de dados de um tipo especifico que podem ser acessados pelo seu index, sempre
+ * iniciando em zero 0.
+ */
+public class VectorsAndArrays {
+
+  /**
+   * Declarando um vetor com 365 espaços alocados na memoria.
+   * <p>
+   * Cada espaço, por padrão, se inicializa, com o valor referente ao seu tipo de dado.
+   */
+  int[] daysOfYear = new int[365];
+}
